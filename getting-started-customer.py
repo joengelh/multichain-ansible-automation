41ae8b9398d5cc8e0d690036fdea63eb13a28126
joengelh <73387330+joengelh@users.noreply.github.com>
2021-08-30 18:22:15 +0200

    prepare setup task

--- getting-started-customer.py
+++ getting-started-customer.py
@@ -4,10 +4,13 @@ client = mcrpc.RpcClient('192.168.2.11','7755','multichainrpc','CbBLBkWNvtJtesii
 
 #print(client.getinfo())
 #client.create("steam","iot001",False)
-#client.issue(client.listaddresses()[0]['address'],"hpeserver",1)
+client.issue(client.listaddresses()[0]['address'],"hitachiserver", 1, {"open":True},)
+#print(client.getmultibalances())
+#client.issuemore(client.listaddresses()[0]['address'],"dellserver",1)
+#print(client.getmultibalances())
 #print(client.listaddresses())
-print(client.listassets())
-client.send("1XXXXXXWrHXXXXXXQtXXXXXXYcXXXXXXZaY3hj","60-265-64467","1")
+#print(client.listassets())
+#client.send("1XXXXXXWrHXXXXXXQtXXXXXXYcXXXXXXZaY3hj","60-265-64467","1")
 
 
 
--- getting-started-serviceProvider.py
+++ getting-started-serviceProvider.py
@@ -2,9 +2,10 @@ import mcrpc
 
 client = mcrpc.RpcClient('192.168.2.8','7755','multichainrpc','HQYxx31kGsdUsV5Q2THPkpbzUyBaNe5i2Xv6ZW7MiMLe')
 
-client.grant("1XXXXXXWrHXXXXXXQtXXXXXXYcXXXXXXZaY3hj","receive")
+#client.grant("1XXXXXXWrHXXXXXXQtXXXXXXYcXXXXXXZaY3hj","receive")
 #client.create("steam","iot001",False)
 #client.issue(client.listaddresses()[0]['address'],"dellserver",600,1)
-print(client.listassets())
-print(client.sendasset("1XXXXXXWrHXXXXXXQtXXXXXXYcXXXXXXZaY3hj","65-266-2074",1))
+print(client.getmultibalances())
+#print(client.sendasset("1XXXXXXWrHXXXXXXQtXXXXXXYcXXXXXXZaY3hj","65-266-2074",100))
+
 
