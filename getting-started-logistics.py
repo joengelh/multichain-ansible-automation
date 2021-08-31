591c084df003c645df5c92db88ac8fc7455c1304
joengelh <73387330+joengelh@users.noreply.github.com>
2021-08-31 11:31:29 +0200

    cleanup leftover code

--- getting-started-logistics.py
+++
@@ -1,5 +0,0 @@
-import mcrpc
-
-client = mcrpc.RpcClient('192.168.2.13','7755','multichainrpc','25XYhe2UiJbzLVhd9TXfX9r2fHtCHtZx4DNLc1mcgUCA')
-
-client.getinfo()
--- getting-started-oem.py
+++
@@ -1,5 +0,0 @@
-import mcrpc
-
-client = mcrpc.RpcClient('192.168.2.2','7755','multichainrpc','BzKimVP5z5zpsWLUN4yEQ9JnkW6qdfvjkvEtCadZZDE3')
-
-print(client.getinfo())
